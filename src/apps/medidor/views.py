7fdf10fce1edb929387e654d1af3d041c4fd021d
Fede A <aguirrefederico@chaco.gob.ar>
2022-11-16 11:59:11 -0300

    añadiendo busqueda de medidor

--- src/apps/medidor/views.py
+++ src/apps/medidor/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from apps.utils.mixins import AdminRequiredMixin
-from django.views.generic.edit import CreateView,UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.urls import reverse, reverse_lazy
 from django.http import HttpResponseRedirect
 from .models import Medidor
 from .forms import RegisterMedidorForm
+
 
 class CrearMedidorView(LoginRequiredMixin, AdminRequiredMixin, CreateView):
     model = Medidor
@@ -24,22 +25,27 @@ class CrearMedidorView(LoginRequiredMixin, AdminRequiredMixin, CreateView):
         context['sidebar_active'] = 'medidores'
         context['sidebar_active_sub'] = 'crear'
         return context
-    
+
 
 class ListMedidoresView(LoginRequiredMixin, AdminRequiredMixin, ListView):
-
     model = Medidor
     template_name = "medidor/lista_medidores.html"
-    context_object_name = 'medidor'
+    context_object_name = 'equipos'
 
     def get_context_data(self, **kwargs):
         ctx = super(ListMedidoresView, self).get_context_data(**kwargs)
         ctx['sidebar_active'] = 'medidores'
         ctx['sidebar_active_sub'] = 'listar'
-        equipos = Medidor.objects.all()
-        ctx['equipos'] = equipos
+        ctx['search'] = self.request.GET.get('search', '')
         return ctx
-    
+
+    def get_queryset(self):
+        query = Medidor.objects.all()
+        search = self.request.GET.get('search', '')
+        if len(search) > 0:
+            query = query.filter(nro_serie__nro_serie__icontains=search)
+        return query.order_by('nro_serie')
+
 
 class EditarMedidorView(LoginRequiredMixin, AdminRequiredMixin, UpdateView):
     model = Medidor
@@ -60,8 +66,9 @@ class EditarMedidorView(LoginRequiredMixin, AdminRequiredMixin, UpdateView):
     def get_form_kwargs(self):
         kwargs = super(EditarMedidorView, self).get_form_kwargs()
         return kwargs
-    
+
+
 def eliminar_medidor(request, pk):
-    medidor = Medidor.objects.get(id = pk)
+    medidor = Medidor.objects.get(id=pk)
     medidor.delete()
     return HttpResponseRedirect(reverse("medidor:listar_medidores"))
